5b7e2f95e34285818f6c0f157a1d618834f43b0a
cbarry0720 <chrisb0720@gmail.com>
2022-07-17 22:03:04 -0400

    added image upload

--- fitness-app/src/screens/PostScreen/PostScreen.js
+++ fitness-app/src/screens/PostScreen/PostScreen.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
-import { Text, View, Button, TouchableOpacity} from 'react-native'
+import { Text, View, Button, TextInput, Image} from 'react-native'
 import styles from './styles';
 import { ActionSheetIOS } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
+import exercisesJSON from "../../../exercises.json";
+import ExerciseSearchList from '../../components/ExerciseSearchList/ExerciseSearchList';
+import * as ImagePicker from "expo-image-picker";
+import { storage, auth } from '../../firebase/config';
+import { ref, uploadBytesResumable } from 'firebase/storage';
+import { Feather } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler';
 
 export default function PostScreen({navigation}) {
 
+    //
+    const [images, setImages] = useState([]);
+
     //state for selected workout
-    const [template, setTemplate] = useState(0);
+    const [workout, setWorkout] = useState({});
+
+    //state for changing exercise search list
+    const [exercisesSearched, setExercisesSearched] = useState([]);
+
+    const [imageUploadIndex, setImageUploadIndex] = useState(0);
 
     //mock workout data
     const workoutList = [
@@ -26,7 +41,7 @@ export default function PostScreen({navigation}) {
         {
             name:"My Arms Day",
             exercises:["Curls", "More Curls", "Even More Curls", "Tricep Extensions Too"]
-        }
+        },
     ]
     workoutList.unshift({name:"New Workout", exercises: []})
 
@@ -42,21 +57,112 @@ export default function PostScreen({navigation}) {
               destructiveButtonIndex: 1
             },
             buttonIndex => {
-              setTemplate(buttonIndex)
+                buttonIndex != 0 ? setWorkout(workoutList[buttonIndex-1]) : ""
             }
           );
     }
 
+    //pressing on exercise within search
+    const selectExercise = (exercise) => {
+        let temp = workout
+        let length = workout.exercises.length
+        temp.exercises[length-1] = exercise.Name
+        setWorkout(temp)
+        setExercisesSearched([])  
+    }
+
     //close workout
-    const endWorkout = () => {
-        setTemplate(0);
+    const endWorkout = async function(){
+        for(let i = 0; i < images.length; i++){
+                setTimeout(() => {uploadImageToStorage(images[i])}, 500*i);
+        }
+        setImages([])
+        setExercisesSearched([])
+        setWorkout({});
     }
     
+    //adding an exercise with "add exercise" button
+    const addExercise = () => {
+        if(workout.exercises.length > 0 && workout.exercises[workout.exercises.length - 1] == null){
+            return
+        }
+        let temp = {
+            name: workout.name,
+            exercises: workout.exercises
+        };
+        temp.exercises.push(null)
+        setWorkout(temp)
+    }
+
+    //on editing exercise search textbox change
+    const exerciseChange = (text) => {
+        if(text.length > 2){
+            setExercisesSearched(exercisesJSON.filter(
+                (x) => {
+                    return x.Name.toLowerCase().indexOf(text.toLowerCase()) != -1;
+                }
+            ))
+        }else{
+            setExercisesSearched([])
+        }
+    }
+    
+    //choose file to upload
+    const chooseFile = () => {
+        ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 0,
+        }).then( result => {
+            if(!result.cancelled){
+                let tempImages = [...images];
+                tempImages.push(result.uri);
+                setImages(tempImages);
+            }
+        });
+    };
+
+   async  function uploadImageToStorage(uri) {
+        console.log("Uploading...")
+        let path = `${Date.now()}-${auth.currentUser.uid}`
+        let reference = ref(storage, path);
+        fetch(uri).then(async function(img){
+            img.blob().then(async function(bytes){
+                uploadBytesResumable(reference, bytes).then(x => {
+                    console.log("Upload Complete!")
+                    return true
+                }).catch(e => {
+                    console.error("error on upload")
+                    console.error(e)
+                });
+                return true
+            }).catch(e => {
+                console.error("error on blob")
+                console.error(e)
+            })
+        }).catch(e => {
+            console.error("error on fetch")
+            console.error(e)
+        })
+        return;
+    }
+
+    const removeImage = (uri) => {
+        return () => {
+            let tempImages = [...images];
+            tempImages = tempImages.filter((x) => {
+                return uri != x
+            })
+            setImages(tempImages)
+        }
+    }
+
     //starting page
-    if(template == 0){
+    if(Object.values(workout).length == 0){
         return (
-            <View>
-                <Button title='Start Workout' onPress={openMenu}/>
+            <View style={styles.startWorkout}>
+                <Button style={styles.startWorkoutButton} title='Start Workout' onPress={openMenu}/>
             </View>
         )
     }
@@ -64,11 +170,11 @@ export default function PostScreen({navigation}) {
     else{
         return (
             <View style={styles.container}>
-                <Text key={workoutList[template-1].name}>{workoutList[template-1].name}</Text>
-                {workoutList[template-1].exercises.map((x) => {
-                    return (
-                        <View style={styles.item}>
-                            <Text style={styles.exercise} key={x}>{x}</Text>
+                <Text style={styles.title} key={workout.name}>{workout.name}</Text>
+                {workout.exercises.map((x) => {
+                    return x != null ? (
+                        <View style={styles.item} key={x}>
+                            <Text style={styles.exercise}>{x}</Text>
                             <View style={styles.right_side}>
                                 <TouchableHighlight style={styles.pr}>
                                     <Text style={{color:"#BA9B00"}}>PR</Text>
@@ -78,12 +184,29 @@ export default function PostScreen({navigation}) {
                                 </TouchableHighlight>
                             </View>
                         </View>
+                    ) :
+                    (
+                        <View key={"new exercise"}>
+                            <TextInput style={styles.exerciseInput} onChangeText={exerciseChange} placeholder='Search Exercise'/>
+                        </View>
                     )
                 })}
-                <Button title='Add Exercise'/>
-                <Button title='Add Media'/>
+                <View style={styles.imagesContainer}>
+                    {images.map(x => {
+                        return (
+                        <View style={styles.imageContainer} key={x}>
+                            <Image style={styles.image} source={{uri:x}}/>
+                            <Feather name='x' style={styles.remove} onPress={removeImage(x)}/>
+                        </View>
+                        )
+                    })}
+                </View>
+                {
+                  exercisesSearched.length > 0 ? <ExerciseSearchList exercises={exercisesSearched} setExercise={selectExercise}/> : <></>
+                }
+                <Button onPress={addExercise} title='Add Exercise'/>
+                <Button onPress={chooseFile} title='Add Media'/>
                 <Button onPress={endWorkout} title='End Workout'/>
-                <Text style={styles.timer}>12:34:56</Text>
             </View>
         )
     }
--- fitness-app/src/screens/PostScreen/styles.js
+++ fitness-app/src/screens/PostScreen/styles.js
@@ -5,6 +5,12 @@ export default new StyleSheet.create({
         flex:1,
         flexDirection:"column",
     },
+    startWorkout:{
+        top:"45%",
+    },
+    startWorkoutButton:{
+        fontSize:30
+    },
     item:{
         flexDirection:"row",
         justifyContent: "space-between",
@@ -45,5 +51,44 @@ export default new StyleSheet.create({
     },
     exercise:{
         fontSize:18
+    },
+    exerciseInput:{
+        backgroundColor: "#cccccc",
+        marginLeft:5,
+        marginRight:5,
+        borderRadius:10,
+        padding:10,
+        fontSize:18
+    },
+    title:{
+        alignSelf:"center",
+        fontSize:24,
+        fontWeight:"bold",
+        padding:10
+    },
+    imagesContainer:{
+        marginTop:10,
+        flex:1,
+        flexDirection: "row",
+        flexWrap:"wrap",
+        alignSelf:"center"
+    },
+    image:{
+        width:125,
+        height:125
+    },
+    imageContainer:{
+
+    },
+    remove:{
+        position:"absolute",
+        padding:2.5,
+        borderWidth:1,
+        borderColor:"black",
+        color:"red",
+        borderStyle:"solid",
+        fontSize:20,
+        left:98,
+        backgroundColor:"white"
     }
 });
